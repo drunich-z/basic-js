fd563042dd9129da4baf74891444fbc1d8365ed2
drunich-z <zverev-and@yandex.ru>
2021-03-25 00:56:49 +0700

    feat: add 1-2 function

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  let k = 0.693 / HALF_LIFE_PERIOD;
+  let N = parseFloat(sampleActivity);
+  if (typeof sampleActivity === "string" && N > 0 && N <= MODERN_ACTIVITY ) {
+    result = Math.log(MODERN_ACTIVITY / N) / k;
+    result = Math.ceil(result);
+    return result
+  } else {
+    return false;
+  }
 };
--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(array) {
+  let count = 0;
+  if (!array) return 0;
+  array.forEach((element) => {
+    element.forEach((elem) => {
+      if (elem == "^^") count += 1;
+    });
+  });
+  return count;
 };
