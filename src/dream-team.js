b31b008da688118abd48bc6cff958a543126737d
drunich-z <zverev-and@yandex.ru>
2021-03-25 21:41:38 +0700

    feat: add 3-4 function

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members) || members.length == 0) return false;
+  
+  let onlyStringArray = members.filter((element) => {
+    return typeof (element) === 'string';
+  });
+
+  if (onlyStringArray.length == 0) return false;
+  
+  let firstLettersArray = onlyStringArray.map((element) => {
+    return element.trim()[0].toUpperCase();
+  }); 
+  
+  return firstLettersArray.sort().join(''); 
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (!date) return 'Unable to determine the time of year!';
+  if(Object.prototype.toString.call(date) === "[object Date]") {
+    let seasons = {
+      1: "winter",
+      2: "winter",
+      3: "spring", 
+      4: "spring", 
+      5: "spring", 
+      6: "summer",
+      7: "summer",
+      8: "summer",
+      9: "autumn", 
+      10: "autumn",
+      11: "autumn",
+      12: "winter"
+    };
+    return seasons[date.getMonth()+1];
+  } else  {
+    throw Error;
+  };
 };
