cb32cd0346961d0cb54724d16a7d90e14df3f28f
drunich-z <zverev-and@yandex.ru>
2021-03-28 02:57:10 +0700

    feat: add 6-7 tasks

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,41 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  arr: [],
+
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.arr.length;
   },
+
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.arr.push(`( ${value} )`);
+    return this;
   },
+
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (
+      typeof position !== "number" ||
+      position < 1 ||
+      position > this.arr.length - 1
+    ) {
+      this.arr = [];
+      throw Error;
+    } else {
+      this.arr.splice(position - 1, 1);
+      return this;
+    }
   },
+
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.arr.reverse();
+    return this;
   },
+
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    resultArr = this.arr.slice();
+    this.arr.length = 0;
+    return resultArr.join("~~");
+  },
 };
 
 module.exports = chainMaker;
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,34 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw Error;
+  let resultArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == "--discard-next") {
+      i++;
+      continue;
+    }
+
+    if (arr[i] == "--discard-prev" && arr[i - 2] == "--discard-next") continue;
+    if (arr[i] == "--discard-prev") {
+      resultArray.pop();
+      continue;
+    }
+
+    if (arr[i] == "--double-next" && i == arr.length - 1) continue;
+    if (arr[i] == "--double-next") {
+      resultArray.push(arr[i + 1]);
+      continue;
+    }
+
+    if (arr[i] == "--double-prev" && i == 0) continue;
+    if (arr[i] == "--double-prev" && arr[i - 2] == "--discard-next") continue;
+    if (arr[i] == "--double-prev") {
+      resultArray.push(arr[i - 1]);
+      continue;
+    }
+
+    resultArray.push(arr[i]);
+  }
+  return resultArray;
 };
