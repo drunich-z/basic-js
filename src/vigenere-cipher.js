cbe0106935d74cacce4ee7888ddc3106373972a4
drunich-z <zverev-and@yandex.ru>
2021-03-30 03:54:01 +0700

    feat: add 10 function

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,56 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct;
+  }
+
+  encrypt(word, key) {
+    if (!word && !key) throw Error();
+    word = word.toUpperCase();
+    key = key.toUpperCase();
+    let result = [];
+    for (let i = 0, j = 0; i < word.length; i++) {
+      if (word[i].match(/[A-Z]/)) {
+        result.push(
+          String.fromCharCode(
+            ((word[i].charCodeAt(0) -
+              "A".charCodeAt(0) +
+              (key[j % key.length].charCodeAt(0) - "A".charCodeAt(0))) %
+              26) +
+              "A".charCodeAt(0)
+          )
+        );
+        j++;
+      } else {
+        result.push(word[i]);
+      }
+    }
+    return this.direct ? result.join("") : result.reverse().join("");
+  }
+
+  decrypt(word, key) {
+    if (!word && !key) throw Error();
+    word = word.toUpperCase();
+    key = key.toUpperCase();
+    let result = [];
+    for (let i = 0, j = 0; i < word.length; i++) {
+      if (word[i].match(/[A-Z]/)) {
+        result.push(
+          String.fromCharCode(
+            ((word[i].charCodeAt(0) -
+              "A".charCodeAt(0) +
+              (26 - (key[j % key.length].charCodeAt(0) - "A".charCodeAt(0)))) %
+              26) +
+              "A".charCodeAt(0)
+          )
+        );
+        j++;
+      } else {
+        result.push(word[i]);
+      }
+    }
+    return this.direct ? result.join("") : result.reverse().join("");
   }
 }
-
 module.exports = VigenereCipheringMachine;
